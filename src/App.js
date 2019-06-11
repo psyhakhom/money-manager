0a3d10d8e7aa2ce9b1d8a88d177dedbcd248a34b
Phanousit Syhakhom <psyhakhom@gmail.com>
2019-06-11 11:14:20 -0400

    added updated files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchData} from './actions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Layout from './components/Layout/Layout';
+import MainNav from './components/MainNav/MainNav'
+
+class App extends Component { 
+  componentDidMount() {
+    //get data from data.json
+    this.props.fetchData();
+  }
+  render() {
+    const data = this.props.setData;
+    console.log('data', data)
+
+    const mappedData = data.map(ele => {
+      return ele.accounts.account.map(items => items)
+    })
+
+    return (
+      <div>
+        <Layout>
+          <MainNav accountInfo={mappedData} />
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const {setData} = state;
+  return {setData}
+}
+
+export default connect(mapStateToProps, {fetchData})(App);
